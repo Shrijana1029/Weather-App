780a205d27d11cf9b8b55cbb134b7504b91a89bc
Shrijana Maharjan <120613068+Shrijana1029@users.noreply.github.com>
2024-09-16 23:20:18 -0700

    added real weather forecast data

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -22,33 +22,35 @@ class _WeatherScreenState extends State<WeatherScreen> {
     getCurrentWeather();
   }
 
-  Future <void> getCurrentWeather() async {
+  Future <Map<String, dynamic>> getCurrentWeather() async {
 
     try{
       String cityName = "London";
       //res contains all data that has been requested
       final res = await http.get(
-        Uri.parse("https://api.openweathermap.org/data/2.5 /forecast?q=$cityName&APPID=$openWeatherApiKey"
+        Uri.parse("https://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$openWeatherApiKey"
         ),
       );
       // to check if PI key is working or not
-      print(res.statusCode);
+      // print(res.statusCode);
       // print(res.body);
 
 
       //jsonDecode decodes the json format data into dart map or list
       final data = jsonDecode(res.body);
+
       if(data['cod'] != '200'){
         throw 'An unexpected error occurred';
       }
-      setState(() {
-        temp = data['list'][0]['main']['temp'];
-      });
+      //returns data to FutureBuilder
+      return data;
+      // temp = data['list'][0]['main']['temp'];
 
     }
     catch(e){
       throw e.toString();
     }
+
   }
 
   @override
@@ -68,21 +70,43 @@ class _WeatherScreenState extends State<WeatherScreen> {
           ,),
         centerTitle: true,
       ),
-          body: temp ==0 ? const CircularProgressIndicator(): Padding(
+      body: FutureBuilder(
+
+        future: getCurrentWeather(),
+        builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return const Center(child: CircularProgressIndicator());
+            }
+            else if(snapshot.hasError)
+              {
+                return Center(child: Text("Error: ${snapshot.error}"));
+              }
+            //holds data from async operation
+            //! means i am sure snapshot.data is not a null
+            //snapshot.data gets data from Future okay..(i.e return)
+            final data = snapshot.data!;
+            final currentWeatherData = data['list'][0];
+            final currentTemp = currentWeatherData['main']['temp'];
+            final currentSky =currentWeatherData['weather'][0]['main'];
+            final currentPressure = currentWeatherData['main']['pressure'];
+            final currentHumidity = currentWeatherData['main']['humidity'];
+            final currentWind = currentWeatherData['wind']['speed'];
+
+
+          return Padding(
             padding: const EdgeInsets.all(12.0),
-            child:   SingleChildScrollView(
+            child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  //THIS IS 1ST CONTAINER
-                   SizedBox(
-                     //to take max amount of widths
-                    width: double.infinity,
-                    child:  Card(
+                  // THIS IS 1ST CONTAINER
+                  SizedBox(
+                    width: double.infinity,  // To take max amount of widths
+                    child: Card(
                       elevation: 10,
                       shape: RoundedRectangleBorder(
-                        borderRadius:  BorderRadius.circular(20),
+                        borderRadius: BorderRadius.circular(20),
                       ),
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(2),
@@ -90,95 +114,107 @@ class _WeatherScreenState extends State<WeatherScreen> {
                           filter: ImageFilter.blur(sigmaX: 2.0, sigmaY: 3.0),
                           child: Column(
                             children: [
-                            const SizedBox(height: 16,),
-                              Text("$temp K", style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold
+                              SizedBox(height: 16),
+                              Text(
+                                "$currentTemp K",
+                                style: const TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                              ),
-                             const SizedBox(height: 16,),
-                             const Icon(Icons.cloudy_snowing, size:65),
-                              SizedBox(height: 16,),
-                              Text("Rain", style: TextStyle(fontSize: 26 ),),
-                              SizedBox(height: 16,),
+                              SizedBox(height: 16),
+                              Icon(
+                                currentSky == 'Clouds' || currentSky == 'Rain' ?
+                                Icons.cloud : Icons.sunny, size: 65),
+                              SizedBox(height: 16),
+                              Text("$currentSky", style: TextStyle(fontSize: 26)),
+                              SizedBox(height: 16),
                             ],
                           ),
                         ),
-                      ) ,
+                      ),
                     ),
                   ),
-                  // tHIS SECOND CONTAINEr
-                const SizedBox(height: 40,),
-                  const Text("Weather Forecast",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
-                 const SizedBox(height: 20),
-                  //SMALL BOXES HARU XAH AI
-                  //row bhitra card xa card bhitra column card is wrapped with sized box
-                 const SingleChildScrollView(
-                   scrollDirection: Axis.horizontal,
-                   child: Row(
+                  // THIS IS THE SECOND CONTAINER
+                  const SizedBox(height: 40),
+                  const Text(
+                    "Weather Forecast",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  const SizedBox(height: 20),
+                  // SMALL BOXES HARU XAH AI
+                  const SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
                       children: [
                         HourlyFocusedItems(
                           icon: Icons.cloudy_snowing,
                           time: '9.0',
                           data: '300.12',
-                        ),HourlyFocusedItems(
+                        ),
+                        HourlyFocusedItems(
                           icon: Icons.cloudy_snowing,
                           time: '10.0',
                           data: '300.44',
-                        ),HourlyFocusedItems(
+                        ),
+                        HourlyFocusedItems(
                           icon: Icons.cloud,
                           time: '11.0',
                           data: '300.76',
-                        ),HourlyFocusedItems(
+                        ),
+                        HourlyFocusedItems(
                           icon: Icons.cloud,
                           time: '12.0',
                           data: '300.11',
-                        ),HourlyFocusedItems(
+                        ),
+                        HourlyFocusedItems(
                           icon: Icons.cloudy_snowing,
                           time: '13.0',
                           data: '201',
-                        ),HourlyFocusedItems(
+                        ),
+                        HourlyFocusedItems(
                           icon: Icons.cloudy_snowing,
                           time: '14.0',
                           data: '201',
                         ),
-
                       ],
                     ),
-                 ),
-                 const SizedBox(height: 40),
-
-                 const  Text("Additional Information" , style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
-                 const  Padding(
-                    padding:  EdgeInsets.all(8.0),
+                  ),
+                  const SizedBox(height: 40),
+                  const Text(
+                    "Additional Information",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
                         AdditionalInfoItem(
                           icon: Icons.water_drop_rounded,
-                          data: 'Humidity',
-                          value: '91',
+                          data: 'Humidit y',
+                          value:'$currentHumidity',
                         ),
                         AdditionalInfoItem(
                           icon: Icons.air,
                           data: 'Wind',
-                          value: '12',
-
+                          value: '$currentWind',
                         ),
                         AdditionalInfoItem(
                           data: 'Pressure',
                           icon: Icons.beach_access_outlined,
-                          value: '22',
-
+                          value: '$currentPressure',
                         ),
-                      ]
+                      ],
                     ),
-                  )
-
-                ],//children
-
                   ),
+                ], // children
+              ),
             ),
-          ),
+          );
+        },
+      ),
+
 
 
     );
